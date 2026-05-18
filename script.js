7eafddbf2b81bfe325b33f823a6c9c72bc2c6cd6
swieczorek <swieczorek@DOM.SCI.EDU.PL>
2026-05-18 13:08:36 +0200

    Final commit

---
+++ script.js
@@ -0,0 +1,8 @@
+rating = document.getElementById("rating");
+submitted = document.getElementById("submitted");
+
+
+function swap() {
+    rating.style.display = "none";
+    submitted.style.display = "flex";
+}
